a350a578c3788ed29ae3973938cb3789765dd0e1
Tri <21522918@gm.uit.edu.vn>
2023-10-05 15:44:53 +0700

    Committing untracked files

---
+++ about/about.js
@@ -0,0 +1,8 @@
+
+let story = document.getElementById("story_holder");
+
+function show_story() {
+    story.setAttribute("style", "display: ;");
+    document.getElementById("story_holder").innerHTML = story;
+    console.log(story);
+}
